df6e6a7148d82bd716104028d307621cedba9759
Adele Teoh <vagrant@vagrant.vm>
2021-01-06 16:55:02 +0000

    Generate day 2 & 3 reports and remove old code

--- produce_summary.py
+++ produce_summary.py
@@ -1,4 +1,3 @@
-
 def create_melon_report(file):
     # open the file 
     the_file = open("um-deliveries-20140519.txt")
@@ -9,63 +8,23 @@ def create_melon_report(file):
         words = line.split('|') 
         # assign the list items to separate variables
         melon, count, amount = words
-        # print a line about the delivery
+        # print a line about the delivery using those variables
         print("Delivered {} {}s for total of ${}".format(
             count, melon, amount))
     # close the file
     the_file.close()
 
-# create new first day report
-
+# day 1 report
+print()
 print("Day 1")
 create_melon_report("um-deliveries-20140519.txt")
 
-# # create first day report
+# day 2 report 
+print()
+print("Day 2")
+create_melon_report("um-deliveries-20140520.txt")
 
-# print("Day 1")
-# the_file = open("um-deliveries-20140519.txt")
-# for line in the_file:       # read each line in the file
-#     line = line.rstrip()    # remove excess whitespace at the end of the line
-#     words = line.split('|') # break the line at pipes and create a "words" list
-
-#     melon = words[0]        # first item in words
-#     count = words[1]        # first item in words >> should be second
-#     amount = words[2]       # first item in words >> should be third
-
-#     print("Delivered {} {}s for total of ${}".format(
-#         count, melon, amount))
-# the_file.close()
-
-
-# # second day report -- identical to first day except for file name
-
-# print("Day 2")
-# the_file = open("um-deliveries-20140520.txt")
-# for line in the_file:
-#     line = line.rstrip()
-#     words = line.split('|')
-
-#     melon = words[0]
-#     count = words[1]
-#     amount = words[2]
-
-#     print("Delivered {} {}s for total of ${}".format(
-#         count, melon, amount))
-# the_file.close()
-
-
-# # third day report -- identical to first day except for file name
-
-# print("Day 3")
-# the_file = open("um-deliveries-20140521.txt")
-# for line in the_file:
-#     line = line.rstrip()
-#     words = line.split('|')
-
-#     melon = words[0]
-#     count = words[1]
-#     amount = words[2]
-
-#     print("Delivered {} {}s for total of ${}".format(
-#         count, melon, amount))
-# the_file.close()
+# day 3 report
+print()
+print("Day 3")
+create_melon_report("um-deliveries-20140521.txt")
