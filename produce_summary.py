151853722c1a7ba49f3781ee372192a00ffb2399
Adele Teoh <vagrant@vagrant.vm>
2021-01-06 16:38:47 +0000

    Fix melon count and total displaying as melon type, added comments

--- produce_summary.py
+++ produce_summary.py
@@ -1,17 +1,21 @@
+# create first day report
+
 print("Day 1")
 the_file = open("um-deliveries-20140519.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
+for line in the_file:       # read each line in the file
+    line = line.rstrip()    # remove excess whitespace at the end of the line
+    words = line.split('|') # break the line at pipes and create a "words" list
 
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
+    melon = words[0]        # first item in words
+    count = words[1]        # first item in words >> should be second
+    amount = words[2]       # first item in words >> should be third
 
     print("Delivered {} {}s for total of ${}".format(
         count, melon, amount))
 the_file.close()
 
+
+# second day report -- identical to first day except for file name
 
 print("Day 2")
 the_file = open("um-deliveries-20140520.txt")
@@ -20,13 +24,15 @@ for line in the_file:
     words = line.split('|')
 
     melon = words[0]
-    count = words[0]
-    amount = words[0]
+    count = words[1]
+    amount = words[2]
 
     print("Delivered {} {}s for total of ${}".format(
         count, melon, amount))
 the_file.close()
 
+
+# third day report -- identical to first day except for file name
 
 print("Day 3")
 the_file = open("um-deliveries-20140521.txt")
@@ -35,8 +41,8 @@ for line in the_file:
     words = line.split('|')
 
     melon = words[0]
-    count = words[0]
-    amount = words[0]
+    count = words[1]
+    amount = words[2]
 
     print("Delivered {} {}s for total of ${}".format(
         count, melon, amount))
